eacaeddfa2d333f22054dec807f512c4d30b3753
Bryan Bergo <bryan.l.bergo@gmail.com>
2023-08-07 16:33:37 -0700

    basic ping example

--- template/bot.js
+++ template/bot.js
@@ -1,17 +1,37 @@
-const { Client, GatewayIntentBits } = require("discord.js");
+const { Client, Collection, Events, GatewayIntentBits } = require("discord.js");
+const ping = require("./ping");
+
+//load env variables
 require("dotenv").config();
 
+//intents represent our permissions, with the current one being
+//the ability to access a Discord Server
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-    GatewayIntentBits.GuildMembers,
-  ],
+  intents: [GatewayIntentBits.Guilds],
 });
 
+//runs once the client is ready to start issuing requests
 client.on("ready", async () => {
- 
+  console.log("Discord Bot Online");
 });
 
+//runs every time an interaction occurs on a server
+client.on(Events.InteractionCreate, (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const command = interaction.client.commands.get(interaction.commandName);
+
+  //cycle through possible commands
+  switch (command) {
+    //commands are placed in `template/commands.js` for readability
+    case "ping":
+      ping.execute(interaction);
+      break;
+    default:
+      interaction.reply("Command not found");
+      break;
+  }
+});
+
+//authenticate our bot with Discord
 client.login(process.env.CLIENT_TOKEN);
